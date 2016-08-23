ce080e9f8eba83e2235c2d7ebe82fa6429e64a0c
thiago.michelini <thiago.michelini@redetendencia.com.br>
2016-08-23 09:31:48 -0400

    #commit máquina do trabalho

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ $(function() {
         dadosUsuario.nome = $('#tx_usuario').val();
         //dadosUsuario.dtHrSessaoGravada = new Date();
         dadosUsuario.minutosSessao = 15;
-    	localStorage.setItem('tnd-user-session', JSON.stringify(dadosUsuario));
+    	//localStorage.setItem('tnd-user-session', JSON.stringify(dadosUsuario));
+        setCookie('tnd-user-session', JSON.stringify(dadosUsuario), dadosUsuario.minutosSessao);
     	window.location = getQueryStringByName('redirect');
     })
 });
@@ -17,4 +18,36 @@ function getQueryStringByName(name, url) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function setCookie(cname, cvalue, exminu) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exminu*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; path=/ ;" + expires + "; domain=" + obterDomain();
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function obterDomain() {
+    var result = window.location.host;
+    var ehIp = Number(result.substring(0, result.indexOf('.')));
+    if (!isNaN(ehIp))
+        return result;
+
+    result = result.substring(result.indexOf('.'), result.length);
+    return result;
 }
--- js/header-apps.js
+++ js/header-apps.js
@@ -1,11 +1,24 @@
 var baseUrl = window.location.protocol + '//' + window.location.host;
+var ambientes = ['dev','int','sit','uat'];
 
 $(function(){
 	var dependencias = '<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">' +
-		'<script src="' + baseUrl + '/portal/js/jquery.popupoverlay.js"></script>';
+		'<script src="' + criarUrl('portal', '/portal') + '/js/jquery.popupoverlay.js"></script>';/* +
+		'<script src="' + criarUrl('portal', '/portal') + '/js/jquery.ajax-cross-origin.min.js"></script>';*/
 	$(dependencias).insertAfter($('#app-config-js'));
+	// var _url = window.location.protocol + '//cors-anywhere.herokuapp.com/' + criarUrl('portal', '/portal') + '/header-apps.html';
+	var _url = 'http://www.corsproxy.com/portal.novaarquitetura.com.br/header-apps.html';
 	setTimeout(function(){
-		$('#header-apps').load(baseUrl + '/portal/header-apps.html', callbackHeader);
+		//$('#header-apps').load(criarUrl('portal', '/portal') + '/header-apps.html', callbackHeader);
+		$.ajax({
+			// dataType: 'text/html',
+			// crossOrigin: true,
+			url: _url,
+			success: function(data) {
+				$('#header-apps').html(data);
+				callbackHeader();
+			}
+		});
 	}, 500);
 });
 
@@ -25,7 +38,7 @@ function definirEventos() {
 	});
 	$('#user-sair').click(function(){
 		localStorage.removeItem('tnd-user-session');
-		window.location = baseUrl + '/portal';
+		window.location = criarUrl('portal', '/portal');
 	});
 }
 
@@ -36,9 +49,9 @@ function desenharHeader() {
 	}
 	$('#lb_nome_app').html(NOME_APLICACAO);
 
-	$('#img-logo-header').attr('src', baseUrl + '/portal/img/img_logo_header.png');
-	$('#img-logo-sgv').attr('src', baseUrl + '/portal/img/sgv_32.png');
-	$('#img-logo-sgr').attr('src', baseUrl + '/portal/img/sgr_50.png');
+	$('#img-logo-header').attr('src', criarUrl('portal', '/portal') + '/img/img_logo_header.png');
+	$('#img-logo-sgv').attr('src', criarUrl('portal', '/portal') + '/img/sgv_32.png');
+	$('#img-logo-sgr').attr('src', criarUrl('portal', '/portal') + '/img/sgr_50.png');
 }
 
 function definirTooltips() {
@@ -53,6 +66,41 @@ function definirTooltips() {
 }
 
 function definirLinksApps() {
-	$('#link-sgr').attr('href', baseUrl + '/crud-angular');
-	$('#link-sgv').attr('href', baseUrl + '/front-end');
+	$('#link-sgr').attr('href', criarUrl('crud', '/crud-angular'));
+	$('#link-sgv').attr('href', criarUrl('tutorial', '/front-end'));
+}
+
+function criarUrl(app, uri) {
+	if (urlEhIp())//usuario estah usando ip para acesso a aplicacao
+		return window.location.protocol + '//' + window.location.host + uri;
+	
+	//usuario estah usando DNS para acesso a aplicacao
+	var amb = window.location.host;
+	amb = amb.substring(0, amb.indexOf('.'));
+	$(ambientes).each(function(i, v) {
+		if (amb.indexOf(v) > -1) {
+			app = app + '-' + v + '.';
+		}
+	});
+
+	return window.location.protocol + '//' + app + obterDomain();
+}
+
+function urlEhIp() {
+	var h = window.location.host;
+	var ehIp = Number(h.substring(0, h.indexOf('.')));
+    if (!isNaN(ehIp))
+        return true;
+
+    return false;
+}
+
+function obterDomain() {
+    var result = window.location.host;
+    var ehIp = Number(result.substring(0, result.indexOf('.')));
+    if (!isNaN(ehIp))
+        return result;
+
+    result = result.substring(result.indexOf('.'), result.length);
+    return result;
 }
